8b668b4cd787d5ff3893ed53536f2fda2f39e20d
unknown <kavunv@gmail.com>
2020-06-19 22:30:02 +0300

    complete hw-07

--- src/Components/ContactItem/ContactItem.jsx
+++ src/Components/ContactItem/ContactItem.jsx
@@ -4,14 +4,13 @@ import PropTypes from "prop-types";
 import { Text, Button } from "./ContactItem.styles";
 
 import iconClose from "../../helpers/icon/Delete_icon.svg";
-import { getLoader } from "../../redux/selectors";
-import { useDispatch, useSelector } from "react-redux";
+
+import { useDispatch } from "react-redux";
 import { asyncDeleteContact } from "../../redux/axiosOperations";
 
 const ContactItem = ({ contact }) => {
   const { name, number, id } = contact;
   const dispatch = useDispatch();
-  const spinier = useSelector((state) => getLoader(state));
 
   const remove = () => {
     dispatch(asyncDeleteContact(id));
@@ -19,8 +18,6 @@ const ContactItem = ({ contact }) => {
 
   return (
     <>
-      {spinier && <div>Loadiing...</div>}
-
       <Text>{name}</Text>
       <Text> {number}</Text>
       <Button src={iconClose} onClick={remove}></Button>
--- src/Components/ContactList/ContactList.jsx
+++ src/Components/ContactList/ContactList.jsx
@@ -5,15 +5,18 @@ import { TransitionGroup } from "react-transition-group";
 import { List, Item } from "./ContactList.styles";
 import ContactItem from "../ContactItem/ContactItem";
 import { contactsFiltered } from "../../redux/selectors";
+import { getLoader } from "../../redux/selectors";
+import Spiner from "../Spiner/Spiner";
 
 const ContactList = () => {
   const filteredArr = useSelector((state) => contactsFiltered(state));
-
+  const spinier = useSelector((state) => getLoader(state));
   return (
     <TransitionGroup component={List}>
       {filteredArr.map((contact) => (
         <Item key={contact.id}>
-          <ContactItem contact={contact}></ContactItem>
+          {spinier && <Spiner />}
+          {!spinier && <ContactItem contact={contact}></ContactItem>}
         </Item>
       ))}
     </TransitionGroup>
